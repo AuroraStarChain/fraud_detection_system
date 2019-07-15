e1051e4f0e17a24a5407a2a1f03c6a348e65006c
Rodrigo de Lima Oliveira <rodrigolima82@gmail.com>
2019-07-15 09:58:45 -0300

    Update 04-NeuralNetwork.R

--- 04-NeuralNetwork.R
+++ 04-NeuralNetwork.R
@@ -3,11 +3,6 @@
 # Obs: Caso tenha problemas com a acentuação, consulte este link:
 # https://support.rstudio.com/hc/en-us/articles/200532197-Character-Encoding
 
-# Configurando o diretório de trabalho
-# Coloque entre aspas o diretório de trabalho que você está usando no seu computador
-# Não use diretórios com espaço no nome
-setwd("Z:/Dropbox/DSA/Business-Analytics/R/Cap06")
-getwd()
 
 # Pacotes
 library(dplyr) 
